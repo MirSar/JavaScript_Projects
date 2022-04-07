3b95fe29c3da6c9d7fffa70736e5e25d099f6c5f
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-07 14:44:24 -0700

    learning JS

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,8 +3,15 @@
 /* for multi-line comment use this method
 which is same as CSS method of commenting */
 
-window.alert("Greetings World!");
+//window.alert("Greetings World!");
 
 // another method of displaying text
-document.write("Hi again, world"); //this writes it on the page
+// document.write("Hi again, world"); //this writes it on the page
+
+//assigning variable 
+var A = "This is a string";
+document.write(A);
+
+window.alert(A);
+
 
