951236e3b294a8dc5741b2685c53294f3afcca46
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-09 13:10:46 -0700

    added additional functions
    Basic JS project2 focused on functions

--- Basic JavaScript Projects/Project2_functions/js/main.js
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -3,8 +3,15 @@ function modernFontColor(str, color) {
     return '<span style= "color: ' + color + '">' + str + '</span>';
 }
 
-//function that changes colors of a given element
+//function that changes colors of a given element p1
 function changeColor(newColor) {
     var elem = document.getElementById('p1');
     elem.style.color = newColor;
+}
+
+//function for concatenation
+function concatFunction() {
+    var sentence = "I'm learning to ";
+    sentence += "Code with JavaScript!";
+    document.getElementById("concatenate").innerHTML = sentence;
 }
