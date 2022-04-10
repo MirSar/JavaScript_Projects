9dfff3057d08f4f54186ead4e1c0c6e5455e921d
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-10 11:54:23 -0700

    created Project3_math_operators
    JS Course

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,9 @@
+//This is JS code for Project3_math_operations
+
+//using the addEventListener()method to execute a function
+document.getElementById("myBtn").addEventListener("click", add_Function);
+
+function add_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2+2= " + addition;
+}
