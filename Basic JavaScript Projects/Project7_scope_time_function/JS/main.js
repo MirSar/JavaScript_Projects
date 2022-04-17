285431a13dcacf513669338b13edac9ca46adb87
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-17 16:52:23 -0700

    finished the Guessing Game

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -2,45 +2,76 @@
 =================================*/
 
 // Global Variable
-var GA, GB, GC;
-GA = 3;
-GB= "Winner"
-GC = true;
+    var GA, GB, GC;
+    GA = 3;
+    GB= "Winner"
+    GC = true;
 
 //Example 1: using console.log to debug use of local variable
-function A() {
-    var a, b;  //Local variables
-    a = (10 + GA);
-    console.log(a);
-}
-function B() {
-    var c, d;  //Local variables
-    d = (10 + a); //if a variable from function A() is used we get ReferenceError: a is not defined
-    console.log(d);
-}
-try {           //using try and catch method to display the error
-    A();
-    B();  //this function has an error coded 
-}
-catch (err) {         //works with try method to catch and display an error
-    document.getElementById("error1").innerHTML = err;    
-}
+    function A() {
+        var a, b;  //Local variables
+        a = (10 + GA);
+        console.log(a);
+    }
+    function B() {
+        var c, d;  //Local variables
+        d = (10 + a); //if a variable from function A() is used we get ReferenceError: a is not defined
+        console.log(d);
+    }
+    try {           //using try and catch method to display the error
+        A();
+        B();  //this function has an error coded 
+    }
+    catch (err) {         //works with try method to catch and display an error
+        document.getElementById("error1").innerHTML = err;    
+    }
 
 //Example 2: Method Assignment, using Date().getHours()
-function wakeUpCall() {
-    if (new Date().getHours() > 5 && new Date().getHours() < 6) {
-        document.getElementById("m1").innerHTML = "Wake UP for Success!!!";
+    function wakeUpCall() {
+        if (new Date().getHours() > 5 && new Date().getHours() < 6) {
+            document.getElementById("m1").innerHTML = "Wake UP for Success!!!";
+        }
+        else if (new Date().getHours() > 6 && new Date().getHours() < 18) {
+            document.getElementById("m1").innerHTML = "Hard work builds good character";
+        }
+        else {
+            document.getElementById("m1").innerHTML = "I hope you had a successful day.";
+        }
     }
-    else if (new Date().getHours() > 6 && new Date().getHours() < 18) {
-        document.getElementById("m1").innerHTML = "Hard work builds good character";
+
+//Example3: Guessing Game using if, else statement
+    //global varialble
+    var myNum;
+    //functon to create a random number
+    function thinkOfNum() {
+        myNum = Math.floor(Math.random() * 101);
+        document.getElementById("readyToPlay").innerHTML = "I'M READY! and it's a really GOOD Number.";
     }
-    else {
-        document.getElementById("m1").innerHTML = "I hope you had a successful day.";
+    //function to compare your guess with the random num
+    function guessingGame() {
+        var guessNum, standDev, lowNum, highNum; //local variables
+
+        guessNum = parseInt(document.getElementById("guess").value);
+        standDev = parseInt(document.getElementById("sd").value);
+        lowNum = guessNum - standDev;
+        highNum = guessNum + standDev;
+
+        if (myNum >= lowNum && myNum <= highNum) {
+            document.getElementById("gameResult").innerHTML = "You must be a psychic! You guessed right! the number was = " + myNum;
+        }
+        else if (myNum > highNum) {
+            document.getElementById("gameResult").innerHTML ="too low guess again";
+        }
+        else if (myNum < lowNum) {
+            document.getElementById("gameResult").innerHTML ="too high guess again";
+        }
+        else {
+            document.getElementById("gameResult").innerHTML ="You have to Guess to play!";   
+        }
+
     }
-}
 
-
-
+//
   
 
 
