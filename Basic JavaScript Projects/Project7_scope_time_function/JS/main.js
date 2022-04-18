9ec0f4d309bbaf101e96738720faf225196c1cc7
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-17 17:10:17 -0700

    added the final function to Project 7

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -71,7 +71,22 @@
 
     }
 
-//
+//Example4: Time of the day example
+    function time_function() {
+        var Time = new Date().getHours();
+        var Reply;
+
+        if (Time < 12 == Time > 0) {
+            Reply = "It is morning time!";
+        }
+        else if (Time >= 12 == Time < 18) {
+            Reply = "It is afternoon!";
+        }
+        else {
+            Reply = "It is evening time!";
+        }
+        document.getElementById("time_of_day").innerHTML = Reply;
+    }
   
 
 
