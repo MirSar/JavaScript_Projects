f727617a0e877c6e42eb1d67f1b452635115be95
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-04-19 12:37:22 -0700

    project9 slidshow update
    one issue remains

---
+++ Basic JavaScript Projects/Project9_Countdown_Slideshow/JS/main.js
@@ -0,0 +1,58 @@
+/*Project 9: Countdown Slideshow
+==================================*/
+/*/Example 1: Timer that will countdown by secs.
+function countdown() {
+    var seconds = document.getElementById("seconds").value; //user input for duration of time
+
+    function tick() {
+        seconds = seconds - 1; //countdown, decrements
+        timer.innerHTML = seconds; //displays the seconds in <p id=timer>
+        var time = setTimeout(tick, 1000); //program is pausing for 1,000 ms (1sec) before it calls "tick" **must be above if statement to function properly
+        if (seconds == -1) {  //indicates time has run out at zero sec
+            alert("Time's up!"); //displayed when time runs out
+            clearTimeout(time); //clears a timer set with the setTimeout() method.
+            timer.innerHTML = ""; //clears the countdown display area
+        }
+    }
+    tick();
+}  */
+
+//Let's put on a good slideshow! type = Carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous controls
+function plusSlides(n) { //different values for n is given in HTML file
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dots");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";  //important to have space with added class name " active"//className property can retrieve or set a class to HTML elements
+    // using += --> gives a case where you can add a new class to an element and keep the old one
+}
