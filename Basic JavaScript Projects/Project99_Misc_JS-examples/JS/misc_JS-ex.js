f150a459345cbba43deb193c428e16f934546ca7
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-05-11 11:34:28 -0700

    JavaScript Challenges uploaded

---
+++ Basic JavaScript Projects/Project99_Misc_JS-examples/JS/misc_JS-ex.js
@@ -0,0 +1,65 @@
+/*****************************************
+XML variables decalred
+******************************************/
+var XML_Parser, XML_Document;
+//creates a text string
+var Movie = 
+    "<movie>"+ 
+        "<title>Inception</title>" +
+        "<director>Christopher Nolan</director>" +
+        "<starring>Leonardo DiCaprio, Ken Watanabe, Joseph Gordon-Levitt, Marion Cotillard, Elliot Page, Tom Hardy, Cillian Murphy, Tom Berenger, Michael Caine</starring>" +
+        "<year>2010</year>" +
+    "</movie>";
+//creates an XML DOM parser
+XML_Parser = new DOMParser();
+//creates a new XML DOM object using the text string
+XML_Document = XML_Parser.parseFromString(Movie, "text/html");
+
+document.getElementById("XML_Example").innerHTML= XML_Document.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+
+/**********************************************
+For the Form phone number Validation
+***********************************************/
+//using Regex in javascript
+function ValidatePhoneNumber(input_str) {
+    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/; //this is the pattern the phone number is tested to
+    return re.test(input_str);
+};
+
+function validateForm(event) {
+    var phone = document.getElementById('phoneNum').value;//getting the input, phone
+    if (!ValidatePhoneNumber(phone)) { // checking to a pattern
+        document.getElementById('phone_error').classList.remove('hidden'); //if not true - removes class hidden
+    } else {
+        document.getElementById('phone_error').classList.add('hidden'); //adds class hidden
+        alert("validation success")
+    }
+    event.preventDefault(); //so that our program doesn't recieve conflicting commands
+};
+
+document.getElementById('myForm_1').addEventListener('submit', validateForm);
+
+/******************************************
+PopUp Contact form
+*******************************************/
+function openPopUpForm() {
+    document.getElementById("myForm_2").style.display = "block";
+}
+function closePopUpForm() {
+    document.getElementById("myForm_2").style.display = "none";
+}
+
+/******************************************
+slideShow challenge
+*******************************************/
+$(document).ready(function () { //when the document is ready
+   
+    $(".slideShow img:gt(0)").hide(); //selects img elements with index num greater than 0; hide all except for the first image
+
+    setInterval(function () { //iterates through the images every few seconds
+        $('.slideShow :first-child').fadeOut(1300) //first img to fadeOut
+            .next('img').slideDown(1300) //the next img to slideDown
+            //end() resets jQuery's internal reference back to the original selection (first-child being appended to end of .slideShow element)
+            .end().appendTo('.slideShow'); //takes the first img and append it to the end of the stack
+    }, 3000);
+});
